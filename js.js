dcd587790a9079a217a7fcd6db2c9360e586795f
sharvil2011 <98631855+sharvil2011@users.noreply.github.com>
2022-03-05 08:51:15 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,102 @@
+let setup = document.getElementById("setup");
+let delivery = document.getElementById("delivery");
+let url = `https://v2.jokeapi.dev/joke/`;
+var arr = [];
+
+function bringJoke(){
+    const xhr = new XMLHttpRequest();
+    var temp_url = url;
+    if(arr.length===0){
+        temp_url += 'Any';
+    }
+    else{
+        for(let i=0;i<arr.length;i++){
+            if(i==0){
+                temp_url = temp_url+ arr[i];
+            }
+            else{
+            temp_url = temp_url+","+arr[i];
+            }
+        }
+    }
+    xhr.open('GET', temp_url , true);
+    console.log(temp_url);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let json = JSON.parse(this.responseText);
+
+            let setup1 = json.setup;
+            currentSetup = setup1;
+
+            if(typeof(setup1)=="undefined"){
+                bringJoke();
+            }
+            else{
+
+            let delivery1 = json.delivery;
+            currentDelivery  =delivery1;
+
+            setup.innerText = setup1;
+            delivery.innerHTML = delivery1;}
+        }
+        else {
+            console.log("Some error occured")
+        }
+    }
+    xhr.send();
+}
+
+function addPrior(e)
+{
+  // if check
+  if(e.target.checked){
+     addToArray(e.target);
+     bringJoke();
+     console.log(arr);
+  }else{
+     removeToArray(e.target);
+     bringJoke();
+     console.log(arr)
+  }
+}
+var arr = [];
+function addToArray(obj){
+  arr.push(obj.value)
+}
+function removeToArray(obj){
+var index = arr.indexOf(obj.value);
+if (index > -1) {
+   arr.splice(index, 1);
+ }
+}
+
+console.log($("#name").val)
+
+// function addPrior2(e)
+// {
+//   if(e.target.checked){
+//      addToArray(e.target);
+//      bringJoke();
+//      console.log(arr2);
+//   }else{
+//      removeToArray(e.target);
+//      bringJoke();
+//      console.log(arr2)
+//   }
+// }
+// var arr2 = [];
+// function addToArray(obj){
+//   arr2.push(obj.value)
+// }
+// function removeToArray(obj){
+// var index = arr2.indexOf(obj.value);
+// if (index > -1) {
+//    arr2.splice(index, 1);
+//  }
+// }
+
+
+
+
+bringJoke();
+$("#refresh").click(bringJoke);
